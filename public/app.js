52d67d4f393d790c7b24fe04cbba054bdbd6aae5
admin <admin@Happy436.local>
2022-07-14 18:40:50 +0300

    refactor app.js

--- public/app.js
+++ public/app.js
@@ -1,24 +1,26 @@
 document.addEventListener("click", (event) => {
-    const id = event.target.dataset.id;
-    const content = event.target.dataset.title
-    if (event.target.dataset.type === "remove") {
-        remove(id).then(() => {
-            event.target.closest("li").remove();
-        });
-    } 
-    if (event.target.dataset.type === "update") {
-        console.log(content)
-        const text = prompt("Edit message", content)
-        if(text !== null){
-            update(id, text).then(() => {location.reload()});
-        }
-    }
+	const id = event.target.dataset.id;
+	const content = event.target.dataset.title;
+	if (event.target.dataset.type === "remove") {
+		remove(id).then(() => {
+			event.target.closest("li").remove();
+		});
+	}
+	if (event.target.dataset.type === "update") {
+		console.log(content);
+		const text = prompt("Edit message", content);
+		if (text !== null) {
+			update(id, text).then(() => {
+				location.reload();
+			});
+		}
+	}
 });
 
 async function update(id, title) {
-    await fetch(`/${id}&${title}`, { method: "PUT" });
+	await fetch(`/${id}&${title}`, { method: "PUT" });
 }
 
 async function remove(id) {
-    await fetch(`/${id}`, { method: "DELETE" });
+	await fetch(`/${id}`, { method: "DELETE" });
 }
